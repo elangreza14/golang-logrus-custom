96a7837e6a52b4894ecd78ce9fe12098e509625e
elangreza14 <rezaelangerlangga14@gmail.com>
2022-05-07 20:48:48 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/elangreza14/golang-logrus-custom
+
+go 1.18
+
+require (
+	github.com/banzaicloud/logrus-runtime-formatter v0.0.0-20190729070250-5ae5475bae5e
+	github.com/sirupsen/logrus v1.8.1
+)
+
+require golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
---
+++ logger.go
@@ -0,0 +1,89 @@
+package logger
+
+import (
+	"os"
+
+	runtime "github.com/banzaicloud/logrus-runtime-formatter"
+	"github.com/sirupsen/logrus"
+)
+
+type (
+	CustomLogger interface {
+		Info(format string, args ...interface{})
+		Warning(format string, args ...interface{})
+		Error(format string, args ...interface{})
+		Debug(format string, args ...interface{})
+		Trace(format string, args ...interface{})
+	}
+
+	customLogger struct {
+		log *logrus.Entry
+	}
+
+	PrefixLoggerName struct {
+		Title       string
+		Description string
+	}
+)
+
+func NewLogger(setupName []PrefixLoggerName) CustomLogger {
+	formatter := &runtime.Formatter{ChildFormatter: &logrus.TextFormatter{
+		ForceColors:   true,
+		FullTimestamp: true,
+	}}
+	formatter.Line = true
+	logrus.SetFormatter(formatter)
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.InfoLevel)
+	logrus.SetReportCaller(true)
+
+	if len(setupName) > 1 {
+		for i := 0; i < len(setupName[1:]); i++ {
+			logrus.WithField(setupName[i].Title, setupName[i].Description)
+		}
+	}
+
+	return &customLogger{
+		log: logrus.WithField(setupName[0].Title, setupName[0].Description),
+	}
+}
+
+func (c *customLogger) Info(format string, args ...interface{}) {
+	if format != "" {
+		c.log.Infof(format, args)
+	} else {
+		c.log.Info(args)
+	}
+}
+
+func (c *customLogger) Warning(format string, args ...interface{}) {
+	if format != "" {
+		c.log.Warningf(format, args)
+	} else {
+		c.log.Warning(args)
+	}
+}
+
+func (c *customLogger) Error(format string, args ...interface{}) {
+	if format != "" {
+		c.log.Errorf(format, args)
+	} else {
+		c.log.Error(args)
+	}
+}
+
+func (c *customLogger) Debug(format string, args ...interface{}) {
+	if format != "" {
+		c.log.Debugf(format, args)
+	} else {
+		c.log.Debug(args)
+	}
+}
+
+func (c *customLogger) Trace(format string, args ...interface{}) {
+	if format != "" {
+		c.log.Tracef(format, args)
+	} else {
+		c.log.Trace(args)
+	}
+}
